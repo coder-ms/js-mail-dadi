d8324a0c477cc660d05ccb211877f7bc7fbeafbd
coder-ms <marco.sbrascia@gmail.com>
2022-10-13 16:05:31 +0300

    es1 da capo pt2

--- js/script.js
+++ js/script.js
@@ -5,17 +5,14 @@
     Chiedi all’utente la sua email,
     controlla che sia nella lista di chi può accedere,
     stampa un messaggio appropriato sull’esito del controllo.
-
-    Gioco dei dadi
-    Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
-    Stabilire il vincitore, in base a chi fa il punteggio più alto.
 */
 
 
 
 
+
 /*MAIL*/
-var mail = document.getElementById('email').value;
+//var mail = document.querySelector('email').value;
 
 const invitati = ["luigi.rossi@gmail.com", "filippo.neri@gmail.com", "andrea.bianchi@gmail.com", "giuseppe.verdi@gmail.com", "antonio.canova@gmail.com"];
 
@@ -24,21 +21,35 @@ console.log(mail, invitati);
 console.log(invitati.length);
 */
 
+/*Collegamento bottone genera(HTML) alla funzione JS*/
+const bottone = document.getElementById('button');
+
 for(let i = 0; i < invitati.length; i++){
-    if(mail== invitati[i] ){
-        document.getElementById("email").innerHTML = invitati[i];
-        document.write("Mail in elenco, sei dentro");
-    }
-    else{
-        document.getElementById("email").innerHTML = invitati[i];
-        document.write("Mail non in elenco, accomodati pure fuori");
-    }
+    console.log(invitati[i]);
 }
 
-
+/*
+var verifyMail = function (){
+    for(let i = 0; i < invitati.length; i++){
+        if(mail == invitati[i] ){
+            document.getElementById("email").innerHTML = invitati[i];
+            document.write("Mail in elenco, sei dentro");
+        }
+        else{
+            document.write("Mail non in elenco, accomodati pure fuori");
+        }
+    }
+}
+bottone.addEventListener('click', verifyMail);
+*/
 
 
 /*DADI*/
+/*
+    Gioco dei dadi
+    Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+    Stabilire il vincitore, in base a chi fa il punteggio più alto.
+*/
 
 var player1Dice = Math.floor(Math.random() * 6) + 1;
 var player2Dice = Math.floor(Math.random() * 6) + 1;
